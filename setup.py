736fda3f442cfa5593d3a7c4f948b56b1718c4f2
Danny Laghi <danny.laghi@ligo.org>
2019-12-28 23:37:27 +0100

    (Minimally) generalize setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,40 @@
-import numpy
+import numpy, os, re
 from setuptools import setup, find_packages
 from distutils.extension import Extension
 from Cython.Build import cythonize
+
+path = os.path.realpath(__file__)
+try:
+    user = re.search('/Users/(.+?)/', path).group(1)
+except AttributeError:
+    user = ''
 
 ext_modules=[
              Extension("cosmology",
                        sources=["cosmology.pyx"],
                        libraries=["m","lal"], # Unix-like specific
-                       library_dirs = ["/Users/danny/opt/master/lib"],
-                       include_dirs=[numpy.get_include(),"/Users/danny/opt/master/include"]
+                       library_dirs = ["/Users/{0}/opt/master/lib".format(user)],
+                       include_dirs=[numpy.get_include(),"/Users/{0}/opt/master/include".format(user)]
                        )
              ]
 
 setup(
       name = "cosmology",
       ext_modules = cythonize(ext_modules),
-      include_dirs=[numpy.get_include(),"/Users/danny/opt/master/include"]
+      include_dirs=[numpy.get_include(),"/Users/{0}/opt/master/include".format(user)]
       )
 ext_modules=[
              Extension("likelihood",
                        sources=["likelihood.pyx"],
                        libraries=["m","lal"], # Unix-like specific
-                       library_dirs = ["/Users/danny/opt/master/lib"],
-                       include_dirs=[numpy.get_include(),"/Users/danny/opt/master/include"]
+                       library_dirs = ["/Users/{0}/opt/master/lib".format(user)],
+                       include_dirs=[numpy.get_include(),"/Users/{0}/opt/master/include".format(user)]
                        )
              ]
 
 setup(
       name = "likelihood",
       ext_modules = cythonize(ext_modules),
-      include_dirs=[numpy.get_include(),"/Users/danny/opt/master/include"]
+      include_dirs=[numpy.get_include(),"/Users/{0}/opt/master/include".format(user)]
       )
 
